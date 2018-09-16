6df54cd9003822764a9491330b8677605e9d2332
JaburMacbookPro <jaburmail@gmail.com>
2018-09-16 13:02:22 -0300

    Configuracao do Redux Saga

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,10 +1,14 @@
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
 
   const tron = Reactotron
     .configure() //.configure({ host: '192.168.1.1' }) // controls connection & communication settings
     .useReactNative() // add all built-in react native plugins
+    .use(reactotronRedux())
+    .use(sagaPlugin())
     .connect() // let's connect!
 
   console.tron = tron;
